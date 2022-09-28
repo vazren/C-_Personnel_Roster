6954e8895a533900ab45c7d0bcd356ed2e925e64
Lauren <lauren.vazquez13@gmail.com>
2022-09-28 13:19:59 -0600

    Revised main.cpp

--- Personnel_Roster/Personnel_Roster/main.cpp
+++ Personnel_Roster/Personnel_Roster/main.cpp
@@ -69,6 +69,7 @@ int main() {
                 personRoster.ReturnToMenu();
                 break;
             case 6:
+                personRoster.~Roster();
                 exit(0);
                 break;
                 
